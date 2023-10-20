abb98851e079db969b0174f609ae15a881b9841a
Shivji Agnihotri <110963535+Shivjiagnihotri@users.noreply.github.com>
2023-10-20 05:28:48 +0000

    updates old to new code

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,10 @@
-import streamlit as st
 # Tika setup
+import streamlit as st
 from tika import parser
 from transformers import pipeline
 # Streamlit UI
+import time
+import streamlit as st
 import warnings
 warnings.simplefilter('ignore')
 import nltk
@@ -13,21 +15,31 @@ from tika import parser
 from transformers import pipeline
 nltk.download('punkt')
 
-# Set page title and favicon
-st.set_page_config(page_title='My Beautiful Streamlit App', page_icon='🌼')
 
-# Set a title for your app
-st.title('🌼 PDF Summarizer 🌼')
+# [theme]
+# backgroundColor = "#FFFFFF"
 
-# Add a subtitle
-st.header('AI Powered summarization tool by Dotkonnekt 🚀')
 
-# Add text with markdown
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# st.set_page_config(page_title='My Beautiful Streamlit App', page_icon='🌼')
+
+st.title("AI Powered summarization tool by dotkonnekt 🚀")
+# st.title('PDF Summarizer')
+
+st.header(' \n')
+# st.header('AI Powered summarization tool by Dotkonnekt')
+
 st.markdown('''📣 Are you tired of reading through lengthy PDFs? Do you wish you could get the gist of a document in just a few sentences? Look no further! The PDF Summarizer Tool by DotKonnekt is here to revolutionize the way you consume information.
 
 This innovative tool uses advanced AI algorithms to extract key points from your PDFs, providing you with a concise summary in seconds. No more sifting through pages of text - get the information you need quickly and efficiently.
-
-Whether you're a student trying to study for an exam, a professional preparing for a meeting, or just someone who loves to learn, this tool is perfect for you. 
 
 Here's what makes our tool stand out:
 - 🚀 **Fast**: Get your summaries in seconds.
@@ -37,7 +49,7 @@ Here's what makes our tool stand out:
 
 So why wait? Give the PDF Summarizer Tool by DotKonnekt a try today and experience the future of reading! 🎉😊👍💻📊🚀''')
 
-# Summarization code
+# global summary_length
 global summarizer
 summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")  
 
@@ -49,7 +61,6 @@ def extract_doc_text(pdf_path):
     document_text = re.sub('([ \t]+)|([\n]+)', lambda m: ' ' if m.group(1) else '\n', document_text)
   except:
     document_text = extract_text(pdf_path, caching=True, codec='utf-8')
-    st.write("Supported documents are PDF, Docx and txt!")
 
   return document_text
 
@@ -76,8 +87,6 @@ def prep_b4_save(text):
     text = re.sub('couldnt', 'couldn\'t', text)
     text = re.sub('theyll', 'they\'ll', text)
     text = re.sub('youd', 'you\'d', text)
-    # text = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', text)
-
     return text
 
 # This function split a huge corpus of text into small chunks or portions
@@ -109,25 +118,24 @@ def text_chunking(new_text):
     chunks = 0
   return chunks
 
-
-
-# This function takes in all the chunks, find the summary of each chunk and return all the summaries of chunks in list form.
+# This function takes in all the chunks, find the summary of each chunk.
 def transformers_summary(chunks):
-    st.write("Summarizing the text. Please wait .......")
     global all_transformers_summaries
     # Pre-allocate summaries list
     count = 1
+    bar = st.progress(0)
     all_transformers_summaries = [{} for i in range(len(chunks))]
-    # Allow user to set summary length
-    summary_length = st.slider("Select summary length", min_value=30, max_value=150, value=30)
-    for chunk in chunks: #for chunk in tqdm(chunks, desc ="Summarizing your document: ")
-
-        # Summarize chunk
+    st.write("Summarizing the text. Please wait .......")
+    
+    for chunk in chunks: 
         try:
-            res1 = summarizer(chunk, max_length=150, min_length= summary_length, do_sample=False)
+            res1 = summarizer(chunk, max_length=150, min_length= 30, do_sample=False)
+            count += 1
+            bar.progress(count - 1)
         except Exception as e:
             # st.write("Skipped chunk", count, "Error:", e)
             count += 1
+            bar.progress(count - 1)
             continue
 
         # Add summary to list
@@ -136,17 +144,18 @@ def transformers_summary(chunks):
 
         all_transformers_summaries[count-1] = res1
 
-        # st.write summary
         # Summarize chunk
         chunk_sum = res1[0]['summary_text']
         sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!)\s', chunk_sum)
         sentences = [sentence.strip().replace(' .', '.') for sentence in sentences]
         bullet_points = [f"• {sentence}" for sentence in sentences]
         bullet_list = "\n".join(bullet_points)
-        st.write(bullet_list)
+        bullet_list = re.sub(r'(?<=\S) *\•', '\n•', bullet_list)
+        st.code(bullet_list)
+        # st.write(bullet_list)
 
-        count += 1
-
+        # count += 1
+    st.write("Done Summarizing!!")
     return all_transformers_summaries
 
 def find_summary_transformers(pdf_path):
@@ -174,46 +183,34 @@ def find_summary_transformers(pdf_path):
     else:
         st.write("Document too short to summarize!")
 
+if 'description' not in st.session_state:
+    st.session_state['description'] = ''
 
-raw_text = '''
-You're about to create your first Streamlit app, or maybe you've found an awesome Streamlit app you want to dive into. In both scenarios, you can't begin building or examine the code without a local development setup.
-What if you could do all of this without the need for a local Python environment?
-Now, it's all in your browser 🪄
-With GitHub Codespaces, you can skip the local environment and enjoy:
-Instant setup: Create, fork, and deploy data apps in a single click.
-Frictionless editing: Explore and debug source code, with libraries pre-configured.
-Develop anywhere: Enjoy the flexibility to build Streamlit apps from any browser.
-There are three ways to use Codespaces: creating a new app, editing an existing app, and forking an existing one.
-#1: Create a new app
-To see it in action, simply log onto Community Cloud and create a new app. (See docs for step-by-step guide.) You can also edit an existing app in your browser.
-How to create a new app with Codespaces in one click
-#2: Fork an existing app
-From any public Streamlit app, click “Fork this app''. Copy the app or explore how it works all within your browser. Then, deploy to Community Cloud to share what you have built!
-You can also fork and spin up a Codespace directly from an app's repository. Just select the "Create codespace on master" button.
-#3 Edit an existing app
-Flexible development is not limited to creating new apps or forking existing ones. Simply select "Edit" in Community Cloud and click the "Create Codespace" button. For a more detailed walkthrough, check out our docs.
-Watch Codespaces in action
-In this video, @DataProfessor puts it all together! Watch step-by-step how you can use GitHub Codespaces to build Streamlit apps in the browser.
-Why Github Codespaces
-We wanted to give developers an in-browser editor that is free, powerful, easy and secure.
-With GitHub Codespaces, you'll have access to
-Ample free tier: Each month, you'll have 60 hours of run time on 2 core Codespaces, plus 15 GB of storage.
-A real Linux operating system: develop and deploy on the same system.
-Seamless tech stack: use the tools you already love, like Visual Studio Code.
-Convenient hosting: Easily host and share your app with Community Cloud.
-GitHub's world class security: have the peace of mind that your code and networks are secure.
-'''
+choice = st.radio(
+    "Choose input type",
+    ('Text field', 'File upload')
+)
 
+if choice == 'Text field':
+    description = st.text_area('Have a story? I will summarize it for you. 😊', value=st.session_state['description'])
+elif choice == 'File upload':
+    uploaded_file = st.file_uploader("**Choose a PDF, Docx or a txt file.**")
+# summary_length = st.slider("Select summary length", min_value=30, max_value=150, value=st.session_state['summary_length'])
 
-# Allow user to upload file
-uploaded_file = st.file_uploader("Choose a file") 
-
-if uploaded_file is not None:
-    file_details = {"Filename":uploaded_file.name, "FileSize":uploaded_file.size}
-    st.write(file_details)
-    # Summarize 
-    summary = find_summary_transformers(uploaded_file.name)
-    # Display summary
-    st.write(summary)
-else:
-   st.write("Please upload a file")
+if st.button('Start Summarization'):
+    if uploaded_file is not None or description:
+        if uploaded_file is not None:
+            file_details = {"Filename":uploaded_file.name, "FileSize":uploaded_file.size}
+            if uploaded_file.name.endswith(('.pdf', '.docx', '.txt')):
+                st.write(file_details)
+                summary = find_summary_transformers(uploaded_file.name)
+            else:
+                st.write("**Supported documents are PDF, Docx and txt!**")
+        if description:
+            if len(description.split()) >= 150:
+                if len(description.split()) < 50000:
+                    summary = transformers_summary(text_chunking(description))
+                else:
+                    st.write("Your text exceeds the 50,000 words limit. Please shorten your text.")
+            else:
+                st.write("**Too short to Summarize!**")
